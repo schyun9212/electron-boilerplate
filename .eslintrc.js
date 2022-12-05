52011f533d17f58ddd0cdf0d467a836ff507671c
schyun9212 <schyun9212@gmail.com>
2022-12-05 16:42:21 +0900

    Add svelte packages, update tsconfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,15 +2,17 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true
+    node: true,
   },
-  extends: 'standard-with-typescript',
-  overrides: [
-  ],
+  extends: "standard-with-typescript",
+  overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
   rules: {
-  }
-}
+    quotes: ["error", "double"],
+    "comma-dangle": "off",
+    semi: "off",
+  },
+};
