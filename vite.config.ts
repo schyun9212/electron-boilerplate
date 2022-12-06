b6c34a78d7caaad358aef50bf3750f3649f9f145
schyun9212 <schyun9212@gmail.com>
2022-12-06 17:29:15 +0900

    Update vite config to reduce output directory

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
@@ -7,8 +8,17 @@ export default defineConfig(({ command, mode }) => {
 
   return {
     plugins: [svelte()],
+    root: "./src/ui",
+    resolve: {
+      alias: [
+        {
+          find: "@views",
+          replacement: path.resolve(__dirname, "./src/ui/views"),
+        },
+      ],
+    },
     build: {
-      outDir: "dist",
+      outDir: "../../dist",
       rollupOptions: {
         input: ["./src/ui/views/main/index.html"],
       },
