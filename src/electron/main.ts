6422dd9c91438f236d07f6bcc747e294d9c96e8b
schyun9212 <schyun9212@gmail.com>
2022-12-07 13:50:53 +0900

    Add electron package and baseline script

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,1 +1,34 @@
-export {};
+/* @ts-ignore */
+import { app, BrowserWindow } from "electron";
+
+let mainWindow: BrowserWindow;
+
+function createWindow() {
+  const win = new BrowserWindow({
+    // resizable: true,
+    width: 1280,
+    height: 720,
+  });
+
+  return win;
+}
+
+app.whenReady().then(() => {
+  mainWindow = createWindow();
+
+  mainWindow.on("closed", () => {
+    mainWindow = undefined;
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (!mainWindow) {
+    mainWindow = createWindow();
+  }
+});
