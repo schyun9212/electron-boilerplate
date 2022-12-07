dea61b48afc1e3a37d0aa156e04fefbcdfeae982
schyun9212 <schyun9212@gmail.com>
2022-12-07 10:48:42 +0900

    Update config to build all views dynamically

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,27 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { readdirSync, lstatSync, existsSync } from "fs";
 
 const UI_WORKSPACE = "./src/ui";
 const OUTPUT_DIR = "../../dist"; /* Relative to UI_WORKSPACE */
 
+function getEntrypoints(filter?: string[]): string[] {
+  const viewDir = `${UI_WORKSPACE}/views`;
+
+  const entrypoints = readdirSync(viewDir).reduce((prev, v) => {
+    if (filter?.length > 0 && !filter.includes(v)) return prev;
+
+    const path = `${viewDir}/${v}/index.html`;
+    const isEntryPoint = existsSync(path);
+    return isEntryPoint ? [...prev, path] : prev;
+  }, [] as string[]);
+
+  return entrypoints;
+}
+
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
-  console.log(mode);
+  // TODO: Determine how to handle
 
   return {
     plugins: [svelte()],
@@ -14,10 +29,7 @@ export default defineConfig(({ command, mode }) => {
     build: {
       outDir: OUTPUT_DIR,
       rollupOptions: {
-        input: [
-          `${UI_WORKSPACE}/views/main/index.html`,
-          `${UI_WORKSPACE}/views/sidebar/index.html`,
-        ],
+        input: getEntrypoints(),
       },
     },
   };
