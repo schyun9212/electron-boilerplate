af5a28db6458a64c67ae110accdc0ddd1cd058d6
schyun9212 <schyun9212@gmail.com>
2022-12-07 10:49:09 +0900

    Squashed commit of the following:
    commit dea61b48afc1e3a37d0aa156e04fefbcdfeae982
    Author: schyun9212 <schyun9212@gmail.com>
    Date:   Wed Dec 7 10:48:42 2022 +0900
    
        Update config to build all views dynamically
    
    commit 05f6128315f4c3ea287b3bc29798adb7057e3980
    Author: schyun9212 <schyun9212@gmail.com>
    Date:   Tue Dec 6 19:01:56 2022 +0900
    
        [WIP] Add sample view and build mutiple entrypoint
    
    commit b6c34a78d7caaad358aef50bf3750f3649f9f145
    Author: schyun9212 <schyun9212@gmail.com>
    Date:   Tue Dec 6 17:29:15 2022 +0900
    
        Update vite config to reduce output directory
    
    commit f89335be622d78e319497c7c54c4059001034131
    Author: schyun9212 <schyun9212@gmail.com>
    Date:   Tue Dec 6 17:19:22 2022 +0900
    
        [WIP] Update build process to build multiple views

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { readdirSync, lstatSync, existsSync } from "fs";
+
+const UI_WORKSPACE = "./src/ui";
+const OUTPUT_DIR = "../../dist"; /* Relative to UI_WORKSPACE */
+
+function getEntrypoints(filter?: string[]): string[] {
+  const viewDir = `${UI_WORKSPACE}/views`;
+
+  const entrypoints = readdirSync(viewDir).reduce((prev, v) => {
+    if (filter?.length > 0 && !filter.includes(v)) return prev;
+
+    const path = `${viewDir}/${v}/index.html`;
+    const isEntryPoint = existsSync(path);
+    return isEntryPoint ? [...prev, path] : prev;
+  }, [] as string[]);
+
+  return entrypoints;
+}
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte()]
-})
+export default defineConfig(({ command, mode }) => {
+  // TODO: Determine how to handle
+
+  return {
+    plugins: [svelte()],
+    root: UI_WORKSPACE,
+    build: {
+      outDir: OUTPUT_DIR,
+      rollupOptions: {
+        input: getEntrypoints(),
+      },
+    },
+  };
+});
