db131439508beb01298226da30f54e6f59c46355
schyun9212 <schyun9212@gmail.com>
2022-12-08 18:58:41 +0900

    [WIP] implementing file protocol

--- src/electron/src/protocol.ts
+++ src/electron/src/protocol.ts
@@ -1,15 +1,62 @@
-import { protocol } from "electron";
-import { normalize } from "path";
+import { session } from "electron";
+import { resolve } from "path";
 
-// TODO: Move to config
-export const FS_PROTOCOL = "file";
+export namespace ProtocolSchemes {
+  export const http = "http";
+  export const file = "file";
+  export const appFile = "app-file";
+}
 
-export function registerFileProtocol(rootDir: string) {
-  protocol.registerFileProtocol(FS_PROTOCOL, (req, callback) => {
-    const url = req.url.substring(FS_PROTOCOL.length + 3);
+type ProtocolCallback = (response: string | Electron.ProtocolResponse) => void;
 
-    return callback({
-      path: normalize(url),
-    });
-  });
+export class FileProtocolService {
+  // TODO: Handle rootdir using interface
+  constructor(private readonly rootDir: string) {
+    this.handleProtocols();
+  }
+
+  private handleProtocols() {
+    const { defaultSession } = session;
+
+    // Block any file:// access
+    defaultSession.protocol.interceptFileProtocol(
+      ProtocolSchemes.file,
+      this.handleFileRequest
+    );
+
+    // Register app-file:// protocol
+    defaultSession.protocol.registerFileProtocol(
+      ProtocolSchemes.appFile,
+      this.handleAppFileRequest
+    );
+  }
+
+  private handleFileRequest(
+    request: Electron.ProtocolRequest,
+    callback: ProtocolCallback
+  ) {
+    // TODO: Print message using log service
+    console.error(
+      `Refused to load resource ${request.url} from ${ProtocolSchemes.file} protocol`
+    );
+
+    return callback({ error: -3 /* ABORTED */ });
+  }
+
+  private handleAppFileRequest(
+    request: Electron.ProtocolRequest,
+    callback: ProtocolCallback
+  ) {
+    const path = request.url.substring(ProtocolSchemes.appFile.length + 3);
+    const resolvedPath = resolve(this.rootDir, path);
+
+    console.log(
+      `Get request ${request} from ${ProtocolSchemes.appFile} protocol`
+    );
+    return callback({ path });
+  }
+
+  public isValidURL(url: string) {
+    return url.startsWith(this.rootDir);
+  }
 }
