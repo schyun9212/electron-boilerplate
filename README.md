3108307bbea2f3f0a47a80f110245e18e89ebd0d
schyun9212 <schyun9212@gmail.com>
2022-12-08 15:02:37 +0900

    Squashed commit of the following:
    commit 90886891ce622b3dd711065e2e0ccc0b8b989271
    Author: schyun9212 <schyun9212@gmail.com>
    Date:   Thu Dec 8 14:36:36 2022 +0900
    
        Fix electron entrypoint
    
    commit 75251a483ff25801c8630df9a2a7368cb89fa9de
    Author: schyun9212 <schyun9212@gmail.com>
    Date:   Thu Dec 8 10:57:55 2022 +0900
    
        Update ui build setting
    
    commit 4e34550d4c814cad884a8935a8850d84cc184d42
    Author: schyun9212 <schyun9212@gmail.com>
    Date:   Thu Dec 8 10:21:04 2022 +0900
    
        [WIP] Update readme and add empty directory for hooks
    
    commit 2bec5d66e244a25c060d152d40144d9b912ee23b
    Author: schyun9212 <schyun9212@gmail.com>
    Date:   Thu Dec 8 10:11:06 2022 +0900
    
        [WIP] rebase directory structure
    
    commit e982ee7d058bac78a25b208a3ddcad992bfd7dc9
    Author: schyun9212 <schyun9212@gmail.com>
    Date:   Thu Dec 8 10:02:36 2022 +0900
    
        Add ui build script, move vite related script to ui directory
    
    commit 70332a8385b55f005a29fa00fb968ee82560de67
    Author: schyun9212 <schyun9212@gmail.com>
    Date:   Wed Dec 7 19:00:22 2022 +0900
    
        Replace project flag to build flag to build all deps
    
    commit 6b01b6824c65f24619f2d7206bf0e4eb7f9154b6
    Author: schyun9212 <schyun9212@gmail.com>
    Date:   Wed Dec 7 14:54:40 2022 +0900
    
        Fix start error
    
    commit 3b1587d51923331f57cdca4bb78bff5ef4fca1c2
    Author: schyun9212 <schyun9212@gmail.com>
    Date:   Wed Dec 7 14:32:50 2022 +0900
    
        Update tsconfig for electron
    
    commit 3244b961454659ef4f2dc38c4662c0c72099e44d
    Author: schyun9212 <schyun9212@gmail.com>
    Date:   Wed Dec 7 14:29:17 2022 +0900
    
        Split tsconfigs to apply per project
    
    commit 6422dd9c91438f236d07f6bcc747e294d9c96e8b
    Author: schyun9212 <schyun9212@gmail.com>
    Date:   Wed Dec 7 13:50:53 2022 +0900
    
        Add electron package and baseline script

---
+++ README.md
@@ -0,0 +1,21 @@
+## TODO
+
+### Setting dev nvironment
+
+- [ ] make dev environment for UI
+- [ ] make dev environment for lib
+- [ ] make dev environment for electron
+- [ ] make dev environment for end-to-end application
+- [ ] implement utility scripts
+
+### Optimization
+
+- [ ] Apply tree-shaking
+
+### Refactoring
+
+- [ ] define how to compose build directory
+
+### Protocol
+
+- [ ] implement virtual file system and protocol
---
+++ src/electron/src/main.ts
@@ -0,0 +1,37 @@
+/* @ts-ignore */
+import { app, BrowserWindow } from "electron";
+
+let mainWindow: BrowserWindow;
+
+function createWindow() {
+  const win = new BrowserWindow({
+    // resizable: true,
+    width: 1280,
+    height: 720,
+    webPreferences: {
+      webSecurity: true,
+    },
+  });
+
+  return win;
+}
+
+app.whenReady().then(() => {
+  mainWindow = createWindow();
+
+  mainWindow.on("closed", () => {
+    mainWindow = undefined;
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (!mainWindow) {
+    mainWindow = createWindow();
+  }
+});
