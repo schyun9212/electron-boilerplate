979acdca0893ddea0e9df8eaf66330bcd5b53f2f
schyun9212 <schyun9212@gmail.com>
2022-12-13 19:23:01 +0900

    [WIP] implementing layout system

--- src/electron/src/lib/view.ts
+++ src/electron/src/lib/view.ts
@@ -1,7 +1,14 @@
-import { BrowserWindow } from "electron";
+import { BrowserView, BrowserWindow } from "electron";
 
-export abstract class LayoutManager {
+export interface LayoutManager {
+  setBounds(bounds: Electron.Rectangle);
+  getBounds(): Electron.Rectangle;
+}
+
+export abstract class Layout implements LayoutManager {
   protected _bounds: Electron.Rectangle;
+
+  public abstract init(): void;
 
   public setBounds(bounds: Electron.Rectangle) {
     this._bounds = bounds;
@@ -12,51 +19,18 @@ export abstract class LayoutManager {
   }
 }
 
-export abstract class Layout extends LayoutManager {
-  public abstract init(): void;
-}
-
-export abstract class View extends LayoutManager {
-  protected _layout: Layout;
+export abstract class View implements LayoutManager {
+  protected _bounds: Electron.Rectangle;
   protected _registerListeners(): void {}
 
   public setBounds(bounds: Electron.Rectangle) {
-    super.setBounds(bounds);
-
-    this._layout.setBounds(bounds);
-    this._layout.init();
-  }
-
-  public abstract destroy(): void;
-}
-
-export class ViewContainer extends LayoutManager {
-  constructor(private _view: View, _bounds?: Electron.Rectangle) {
-    super();
-
-    this.setBounds(_bounds ?? _view.getBounds());
-  }
-
-  public setView(view: View) {
-    this._view.destroy();
-    view.setBounds(this._bounds);
-    this._view = view;
-  }
-
-  public getView(): View {
-    return this._view;
-  }
-
-  public setBounds(bounds: Electron.Rectangle) {
     this._bounds = bounds;
-    this._view.setBounds(bounds);
   }
 
   public getBounds(): Electron.Rectangle {
     return this._bounds;
   }
 
-  public destory(): void {
-    this._view.destroy();
-  }
+  public abstract applyLayout(): void;
+  public abstract destroy(): void;
 }
