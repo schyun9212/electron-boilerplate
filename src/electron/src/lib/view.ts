6e2311c68963fd57e35ee1cb2a326c2f38253802
schyun9212 <schyun9212@gmail.com>
2022-12-14 17:08:49 +0900

    Add new baseline code for layout system

--- src/electron/src/lib/view.ts
+++ src/electron/src/lib/view.ts
@@ -1,36 +1,56 @@
-import { BrowserView, BrowserWindow } from "electron";
+import { BrowserView, BrowserWindow, Rectangle } from "electron";
 
-export interface LayoutManager {
-  setBounds(bounds: Electron.Rectangle);
-  getBounds(): Electron.Rectangle;
+/**
+ * TODO
+ *
+ * 1. Determine the most suitable name not to be confusing with classes in Electron.js
+ *
+ */
+
+// TODO
+// 1. Determine how to handle inheritance of webPreference
+// 2. Determine whether we should have some manager to handle UI tree for modulization
+// 3. Determine propagation policy for events as Component has children e.g. setBounds, ...
+export class View extends BrowserView {
+  public parent: View | undefined = undefined;
+
+  // TODO: Make structure of children to heap for search optimization
+  protected readonly _children: View[] = [];
+
+  constructor(options?: Electron.BrowserViewConstructorOptions) {
+    super(options);
+  }
+
+  children() {
+    return this._children;
+  }
+
+  // TODO
+  // 1. Determine how to handle bounds.
+  // e.g. absolte or relative to parent
+  // 2. Determine how to manage children (insert, delete, modify, search, ...)
+  addChildView(view: View, bounds: Electron.Rectangle) {
+    view.setBounds(bounds);
+    view.parent = this;
+    this._children.push(view);
+  }
+
+  setBounds(bounds: Electron.Rectangle): void {
+    super.setBounds(bounds);
+    this.layout();
+  }
+
+  layout() {}
 }
 
-export abstract class Layout implements LayoutManager {
-  protected _bounds: Electron.Rectangle;
-
-  public abstract init(): void;
-
-  public setBounds(bounds: Electron.Rectangle) {
-    this._bounds = bounds;
+export class Window extends BrowserWindow {
+  addView(view: View): void {
+    this.addBrowserView(view);
+    view.children().forEach((v) => this.addBrowserView(v));
   }
 
-  public getBounds(): Electron.Rectangle {
-    return this._bounds;
+  removeComponent(view: View): void {
+    view.children().forEach((v) => this.removeComponent(v));
+    this.removeComponent(view);
   }
-}
-
-export abstract class View implements LayoutManager {
-  protected _bounds: Electron.Rectangle;
-  protected _registerListeners(): void {}
-
-  public setBounds(bounds: Electron.Rectangle) {
-    this._bounds = bounds;
-  }
-
-  public getBounds(): Electron.Rectangle {
-    return this._bounds;
-  }
-
-  public abstract applyLayout(): void;
-  public abstract destroy(): void;
 }
